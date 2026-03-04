a25967ededc9fb03bae086ee5c76c51df29ac6a7
Chrysr17 <chrisrasaro@gmail.com>
2026-03-04 09:47:56 -0500

    autos - categoria route

---
+++ src/app/api/autos/categoria/route.ts
@@ -0,0 +1,26 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function GET(_req: Request, ctx: { params: { categoria: string } }) {
+  const token = cookies().get("token")?.value;
+
+  if (!token) {
+    return NextResponse.json({ message: "No auth" }, { status: 401 });
+  }
+
+  const gatewayUrl = process.env.NEXT_PUBLIC_API_URL;
+
+  const resp = await fetch(
+    `${gatewayUrl}/api/autos/categoria/${ctx.params.categoria}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      cache: "no-store",
+    }
+  );
+
+  const data = await resp.json();
+
+  return NextResponse.json(data, { status: resp.status });
+}
